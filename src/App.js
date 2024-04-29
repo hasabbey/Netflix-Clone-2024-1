82903f5474d44d49936a884865822b6ed4ec75ed
hasabbey <haseotcnf21@yahoo.com>
2024-04-29 17:37:12 -0500

    axios and requests

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import Home from './Pages/Home/Home';
 function App() {
   return (
     <div className="App">
+
       <Home/>
      
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react';
+import axios from '../../Utils/axios';
+import requests from '../../Utils/requests';
+import url from '../../Utils/url';
+
+const Banner = () => {
+    const [movie, setMovie] = useState({});
+    useEffect(() => {
+        (async () => {
+            try {
+                const request = await axios.get(requests.fetchNetflixOriginals);
+                console.log(request);
+                setMovie(
+                    request.data.results[
+                        Math.floor(Math.random() * request.data.results.length)
+                    ]
+                );
+            } catch (error) {
+                console.log('error', error);
+            }
+        })();
+    }, []);
+    function truncate(str, n) {
+        return str?.length > n ? str.substr(0, n - 1) + "..." : str;
+        
+    }
+    return (
+      <div
+        className="banner"
+        style={{
+          backgroundSize: "cover",
+          backgroundImage: url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}"),
+          backgroundPosition: "center center",
+          backgroundRepeat: "no-repeat",
+          height: "63vh",
+          color: "white",
+        }}
+      >
+        <div className="content">
+          <h1 className="title">
+            {movie?.title || movie?.name || movie?.original_name}
+          </h1>
+          <div className="banner_buttons">
+            <button className="button button1" >Play</button>
+            <button className="button button2">My List</button>
+          </div>
+          <h1 className="description">{truncate(movie?.overview, 150)}</h1>
+        </div>
+        <div className="fade_bottom"></div>
+      </div>
+    );
+}
+
+
+export default Banner
