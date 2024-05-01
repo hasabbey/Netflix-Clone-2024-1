27ea7d5518d283449fc869c5cfdeda2b17939ebb
hasabbey <haseotcnf21@yahoo.com>
2024-05-01 12:46:59 -0500

    banner

---
+++ src/Components/Rows/RowList/RowList.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import Row from '../../../Components/Rows/Row/Row'
+import requests from '../../../../src/utils/requests'
+const RowList = () => {
+  return (
+    <>
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow={true}/>
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated}/>
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending}/>
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries}/>
+
+    </>
+  )
+}
+
+export default RowList;
